b25abbdcf421b743554cd2b4bd87f3773df07ed7
gabriellaec <49689481+gabriellaec@users.noreply.github.com>
2021-11-12 14:01:47 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from boto3.session import Session
 import os
 from botocore.errorfactory import ClientError
 import secrets
+import time
 
 #session = Session(region_name='us-east-1')
 ############################################ Precisa mudar o dir
@@ -11,19 +12,26 @@ import secrets
 KEY_DIR = "/home/gabi/Documents/ec2-key-pair-projeto1-gabi.pem"
 KEY_NAME = "ec2-key-pair-projeto1-gabi"
 
+KEY_DIR_NV = "/home/gabi/Documents/ec2-key-pair-projeto1-gabi2.pem"
+KEY_NAME_NV = "ec2-key-pair-projeto1-gabi2"
+
 
 # ********** CONFIGURAÇÕES DAS INSTÂNCIAS ********** #
-AMI_UBUNTU_LTS = "ami-020db2c14939a8efb"
+AMI_UBUNTU_LTS_OHIO = "ami-020db2c14939a8efb"
+AMI_UBUNTU_LTS_NVIR = "ami-0279c3b3186e54acd"
 INSTANCE_TYPE = "t2.micro"
 AMI_NV = "image_client_NV"
 TAG_KEY = "instance-proj"
-TAG_VAL_OHIO = "ohio_psql"
-TAG_VAL_NVIR = "north-virginia-orm"
+TAG_VAL_OHIO = "OhioPsql"
+TAG_VAL_NVIR = "NorthVirginia"
 
 
 # ********** CONFIGURAÇÕES DO SECURITY GROUP ********** #
-SECURITY_GROUP_NAME_OHIO = "SecurityGpOhio"
+SECURITY_GROUP_NAME_OHIO = "SecurityGroupOhio"
 SECURITY_GROUP_DESCRIPTION_OHIO = "Security Group para a instancia de Ohio"
+
+SECURITY_GROUP_NAME_NVIR = "SecurityGroupNVir"
+SECURITY_GROUP_DESCRIPTION_NVIR = "Security Group para a instancia de North Virginia"
 
 
 # ********** boto3 CLIENT & RESOURCES ********** #
@@ -35,29 +43,6 @@ resource_NVIRGINIA = boto3.resource('ec2', region_name='us-east-1')
 
 
 # ********** USER DATA ********** #
-USER_DATA_POSTGRES = f'''#!/bin/bash
-sudo apt update
-sudo apt install postgresql postgresql-contrib -y
-sudo -u postgres psql -c "CREATE USER {secrets.username} WITH PASSWORD '{secrets.password}';"
-sudo -u postgres createdb -O cloud tasks
-sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/10/main/postgresql.conf
-sudo sh -c 'echo "host    all             all             0.0.0.0/0               trust" >> /etc/postgresql/10/main/pg_hba.conf'
-sudo ufw allow 5432/tcp 
-sudo systemctl restart postgresql 
-'''
-
-# sudo nano /etc/postgresql/10/main/pg_hba.conf
-# sudo nano /etc/postgresql/10/main/postgresql.conf
-
-####### ESSE TEM QUE MUDAR !!!
-ohio_instance_ip=0
-USER_DATA_ORM = f'''#!/bin/bash
-git clone https://github.com/gabriellaec/tasks.git
-sed -i "s/'HOST': 'node1'/'HOST': '{ohio_instance_ip}/g" /tasks/portfolio/settings.py
-cd tasks
-./install.sh 
-sudo reboot
-'''
 
 
 
@@ -113,12 +98,27 @@ def create_instance(client, ami, instance_type, key_name, user_data, security_gr
         },
     ]
     )
-    instance_id = instances["Instances"][0]["InstanceId"]
+    instance = instances["Instances"][0]
+    # print("Criando a instância...")
+    # instance.wait_until_running()
+    # instance_state = instances["Instances"][0]["State"]["Name"]
+    # print(instance_state)
+    
+    # print("Criando a instância...")
+    # while instance_state != 'running':
+    #    print(f"Instância está {instance_state}...")
+    #    time.sleep(10)
+    #    instance.update()
+    #    instance_state = instances["Instances"][0]["State"]["Name"]
+
+    instance_id = instance["InstanceId"]
+
     print(f'Instância com id={instance_id} criada com sucesso')
     print(f"Tag com chave={tag_key} e valor={tag_val} adicionada com sucesso à instância {instance_id}")
 
     return instance_id
-
+# Para esperar a instancia estar rodando:
+# https://newbedev.com/ec2-waiting-until-a-new-instance-is-in-running-state
 
 def terminate_instance(ec2_client, ec2_resource, instance_id):
 
@@ -130,7 +130,7 @@ def terminate_instance(ec2_client, ec2_resource, instance_id):
         print()
         print(response)
     else:
-        print("Nenhuma instância com esse id rodando!")
+        print(f"Nenhuma instância com id={instance_id} rodando!")
 
 
 def create_image(ec2_client, instance_id, name):
@@ -156,10 +156,17 @@ def create_security_group(client_ec2, security_gp_name, description):
                 'FromPort': 80,
                 'ToPort': 80,
                 'IpRanges': [{'CidrIp': '0.0.0.0/0'}]},
+                
+                {'IpProtocol': 'tcp',
+                'FromPort': 8080,
+                'ToPort': 8080,
+                'IpRanges': [{'CidrIp': '0.0.0.0/0'}]},
+
                 {'IpProtocol': 'tcp',
                 'FromPort': 22,
                 'ToPort': 22,
                 'IpRanges': [{'CidrIp': '0.0.0.0/0'}]},
+
                 {'IpProtocol': 'tcp',
                 'FromPort': 5432,
                 'ToPort': 5432,
@@ -170,13 +177,36 @@ def create_security_group(client_ec2, security_gp_name, description):
         print("Security Group já existe!")
 
 
-def get_instance_id_by_tag(client_ec2, tag_key, tag_val):
-        custom_filter = [{
-            'Name': tag_key, 
+
+##### https://dashbird.io/blog/boto3-aws-python/
+def get_instance_id_by_tag(client_ec2, tag_val):
+        filter = [{
+            'Name': 'tag:instance-proj', 
             'Values': [tag_val]}]
             
-        response = client_ec2.describe_instances(Filters=custom_filter)
-        return response
+        response = client_ec2.describe_instances(Filters=filter)
+        if response is not None:
+            instanceid = response["Reservations"][0]['Instances'][0]['InstanceId']
+            return instanceid
+
+
+def get_ip_by_id(resource_ec2, instance_id):
+    
+    running_instances = resource_ec2.instances.filter(Filters=[{
+    'Name': 'instance-state-name',
+    'Values': ['pending', 'running']}])
+
+    print("Buscando ip da instância...")
+    for instance in running_instances:
+        if instance.id==instance_id:
+            ip=instance.public_ip_address
+            print(f"Instância com id={instance_id} possui o ip={ip}\n")
+            return ip
+
+
+    
+
+
 #**************************************************#
 # ******************** SCRIPT ******************** #
 #**************************************************#
@@ -184,8 +214,11 @@ def get_instance_id_by_tag(client_ec2, tag_key, tag_val):
 print("----------------------------------------")
 print("---------- COMEÇANDO O SCRIPT ----------")
 print("----------------------------------------")
+print()
+print("----------------- OHIO -----------------")
 
-print("* CRIANDO UM KEY PAIR")
+
+print("* CRIANDO UM KEY PAIR PARA OHIO")
 create_key_pair(client_OHIO, KEY_DIR, KEY_NAME)
 print()
 
@@ -195,16 +228,68 @@ print()
 
 print("* CRIANDO A INSTÂNCIA DE OHIO")
 
-old_ohio_id = get_instance_id_by_tag(client_OHIO, TAG_KEY, TAG_VAL_OHIO)
-if old_ohio_id is not None:
-    terminate_instance(client_OHIO, resource_OHIO, old_ohio_id)
+# old_ohio_id = get_instance_id_by_tag(client_OHIO, TAG_VAL_OHIO)
+# if old_ohio_id is not None:
+#     print(f"id da instância antiga: {old_ohio_id}")
+#     terminate_instance(client_OHIO, resource_OHIO, old_ohio_id)
 
-instance_OHIO_id = create_instance(client_OHIO, AMI_UBUNTU_LTS, INSTANCE_TYPE, KEY_NAME, USER_DATA_POSTGRES, SECURITY_GROUP_NAME_OHIO, TAG_KEY, TAG_VAL_OHIO)
-# add_tags(instance_OHIO_id, resource_OHIO, "instancia1", "OHIO")
+USER_DATA_POSTGRES=f'''#!/bin/bash
+sudo apt update
+sudo apt install postgresql postgresql-contrib -y
+sudo -u postgres psql -c "CREATE USER {secrets.username} WITH PASSWORD '{secrets.password}';"
+sudo -u postgres createdb -O cloud tasks
+sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/10/main/postgresql.conf
+sudo sh -c 'echo "host    all             all             0.0.0.0/0               trust" >> /etc/postgresql/10/main/pg_hba.conf'
+sudo ufw allow 5432/tcp 
+sudo systemctl restart postgresql 
+'''
 
+instance_OHIO_id = create_instance(client_OHIO, AMI_UBUNTU_LTS_OHIO, INSTANCE_TYPE, KEY_NAME, USER_DATA_POSTGRES, SECURITY_GROUP_NAME_OHIO, TAG_KEY, TAG_VAL_OHIO)
+
+instance = resource_OHIO.Instance(id=instance_OHIO_id)
+print("Esperando a instância estar rodando...")
+instance.wait_until_running()
 
 # --------------- Criando a instância de NORTH VIRGINIA --------------- #
-#instance_NVIRGINIA_id = create_instance(client_NVIRGINIA, AMI_UBUNTU_LTS, INSTANCE_TYPE, KEY_NAME, USER_DATA_ORM)
+print()
+print("------------- NORTH VIRGINIA -------------")
+print()
+print("* CRIANDO UM KEY PAIR PARA NORTH VIRGINIA")
+create_key_pair(client_NVIRGINIA, KEY_DIR_NV, KEY_NAME_NV)
+
+
+print("* CRIANDO UM SECURITY GROUP PARA NORTH VIRGINIA")
+create_security_group(client_NVIRGINIA, SECURITY_GROUP_NAME_NVIR, SECURITY_GROUP_DESCRIPTION_NVIR)
+print()
+
+instance_OHIO_ip = get_ip_by_id(resource_OHIO, instance_OHIO_id)
+USER_DATA_ORM=f'''#!/bin/bash
+git clone https://github.com/gabriellaec/tasks.git
+sed -i "s/'HOST': 'node1'/'HOST': '{instance_OHIO_ip}'/g" tasks/portfolio/settings.py
+sudo apt update -y
+sudo apt-get install python3-pip -y
+pip3 install django
+cd tasks
+./install.sh 
+sudo reboot
+        '''
+##### TESTEEE
+# print()
+# print("* CRIANDO A NORTH VIRGINIA")
+# print("** Parâmetros")
+# print("AMI_UBUNTU_LTS_NVIR: ", AMI_UBUNTU_LTS_NVIR)
+# print("INSTANCE_TYPE: ", INSTANCE_TYPE)
+# print("KEY_NAME: ", KEY_NAME)
+# print("USER_DATA_ORM: ", USER_DATA_ORM)
+# print("SECURITY_GROUP_NAME_OHIO: ", SECURITY_GROUP_NAME_OHIO)
+# print("TAG_KEY: ", TAG_KEY)
+# print("TAG_VAL_NVIR: ", TAG_VAL_NVIR)
+# print()
+
+instance_NVIRGINIA_id = create_instance(client_NVIRGINIA, AMI_UBUNTU_LTS_NVIR, INSTANCE_TYPE, KEY_NAME_NV, USER_DATA_ORM, SECURITY_GROUP_NAME_NVIR, TAG_KEY, TAG_VAL_NVIR)
+instance = resource_NVIRGINIA.Instance(id=instance_NVIRGINIA_id)
+print("Esperando a instância estar rodando...")
+instance.wait_until_running()
 
 # INSTALAR O ORM --> MUDAR SCRIPT DO DJANGO
 
@@ -215,6 +300,9 @@ instance_OHIO_id = create_instance(client_OHIO, AMI_UBUNTU_LTS, INSTANCE_TYPE, K
 # terminate_instance(client_NVIRGINIA, instance_NVIRGINIA_id)
 
 
+#### LOAD BALANCER
+# http://boto.cloudhackers.com/en/latest/elb_tut.html
+#https://www.stratoscale.com/knowledge/load-balancing/aws-elb/boto-3-for-elb/example-work-with-a-load-balancer-and-target-group/
 
 
 '''
